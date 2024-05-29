04371cda2d00e18f5f0f547fc67bd74c41e30475
racousin <racousin@student.42.fr>
2024-05-29 11:18:51 +0200

    tp2andtp3

---
+++ thesuperstudent/tp2/mysupertools/tool/multiplication_a_b.py
@@ -0,0 +1,14 @@
+### Content of `multiplication_a_b.py`:
+
+
+def multiply(a, b):
+    try:
+        return float(a * b)
+    except Exception:
+        return "error"
+
+
+if __name__ == "__main__":
+    # Example usage
+    print(multiply(4, 5))
+    print(multiply("a", 3))
