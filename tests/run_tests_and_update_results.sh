d1286f1af850653ec7343e230d1ae16f2e95598c
racousin <racousin@student.42.fr>
2024-05-30 11:36:11 +0200

    up

--- tests/run_tests_and_update_results.sh
+++ tests/run_tests_and_update_results.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-
-# This script runs tests for each TP and updates the user-specific JSON file with the results.
+# This script runs tests for changed TPs and updates the user-specific JSON file with the results.
 
 USERNAME=$1
 AWS_ACCESS_KEY_ID=$2
 AWS_SECRET_ACCESS_KEY=$3
 AWS_DEFAULT_REGION=$4
+CHANGED_TPS=$5  # This is passed from the GitHub Action
 
-# Download the current test results file
-aws s3 cp "s3://www.raphaelcousin.com/students/${USERNAME}.json" "${USERNAME}.json"
+# Check if the user's results file exists; if not, create an empty JSON object
+if ! aws s3 ls "s3://www.raphaelcousin.com/students/${USERNAME}.json" > /dev/null; then
+    echo "{}" > "${USERNAME}.json"
+else
+    aws s3 cp "s3://www.raphaelcousin.com/students/${USERNAME}.json" "${USERNAME}.json"
+fi
 
-for TP in $(ls tests | grep tp); do
-    TP_NUMBER=$(echo $TP | cut -d'p' -f2)
-    TP_TEST_SCRIPT="./tests/$TP/run_tests.sh"
+# Iterate over each changed TP and run tests
+for tp in $CHANGED_TPS; do
+    TP_TEST_SCRIPT="./tests/tp${tp}/run_tests.sh"
 
     if [ -f "$TP_TEST_SCRIPT" ]; then
-        echo "Running tests for $TP for user $USERNAME..."
+        echo "Running tests for TP${tp} for user ${USERNAME}..."
         TEST_SUCCESS=$(bash "$TP_TEST_SCRIPT" "$USERNAME" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" "$AWS_DEFAULT_REGION" && echo "success" || echo "error")
-        jq --arg tp "$TP_NUMBER" --arg result "$TEST_SUCCESS" '.[$tp] = $result' "${USERNAME}.json" > temp.json && mv temp.json "${USERNAME}.json"
+        jq --arg tp "tp$tp" --arg result "$TEST_SUCCESS" '.[$tp] = $result' "${USERNAME}.json" > temp.json && mv temp.json "${USERNAME}.json"
     else
-        echo "Error: Test script does not exist for $TP."
-        exit 1
+        echo "Error: Test script does not exist for TP${tp}."
     fi
 done
 
