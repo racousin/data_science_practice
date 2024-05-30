cc12913b714025e85d00b44e4c4015328c7807a8
racousin <racousin@student.42.fr>
2024-05-30 11:22:17 +0200

    update display results

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# Usage: ./run_tests.sh <username> <tp-number> <aws-access-key-id> <aws-secret-access-key> <aws-region>
-# Example: ./run_tests.sh alice tp1 key secret region
-
-if [ "$#" -ne 5 ]; then
-  echo "Usage: $0 <username> <tp-number> <aws-access-key-id> <aws-secret-access-key> <aws-region>"
-  exit 1
-fi
-
+# This script runs tests for each TP and updates the user-specific JSON file with the results.
 
 USERNAME=$1
-TP_NUMBER=$2
-AWS_ACCESS_KEY_ID=$3
-AWS_SECRET_ACCESS_KEY=$4
-AWS_DEFAULT_REGION=$5
+AWS_ACCESS_KEY_ID=$2
+AWS_SECRET_ACCESS_KEY=$3
+AWS_DEFAULT_REGION=$4
 
-TP_TEST_SCRIPT="./tests/$TP_NUMBER/run_tests.sh"
+# Download the current test results file
+aws s3 cp "s3://www.raphaelcousin.com/students/${USERNAME}.json" "${USERNAME}.json"
 
-if [ -f "$TP_TEST_SCRIPT" ]; then
-  echo "Running tests for $TP_NUMBER for user $USERNAME..."
-  bash "$TP_TEST_SCRIPT" "$USERNAME" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" "$AWS_DEFAULT_REGION"
-else
-  echo "Error: Test script does not exist for $TP_NUMBER."
-  exit 1
-fi
+for TP in $(ls tests | grep tp); do
+    TP_NUMBER=$(echo $TP | cut -d'p' -f2)
+    TP_TEST_SCRIPT="./tests/$TP/run_tests.sh"
+
+    if [ -f "$TP_TEST_SCRIPT" ]; then
+        echo "Running tests for $TP for user $USERNAME..."
+        TEST_SUCCESS=$(bash "$TP_TEST_SCRIPT" "$USERNAME" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" "$AWS_DEFAULT_REGION" && echo "success" || echo "error")
+        jq --arg tp "$TP_NUMBER" --arg result "$TEST_SUCCESS" '.[$tp] = $result' "${USERNAME}.json" > temp.json && mv temp.json "${USERNAME}.json"
+    else
+        echo "Error: Test script does not exist for $TP."
+        exit 1
+    fi
+done
+
+# Upload the updated test results file
+aws s3 cp "${USERNAME}.json" "s3://www.raphaelcousin.com/students/${USERNAME}.json"
