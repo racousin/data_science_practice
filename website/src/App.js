1d6c34833ff4800fd06d089c7ec12ce2e062e4e3
racousin <racousin@student.42.fr>
2024-06-05 16:08:18 +0200

    up

--- website/src/App.js
+++ website/src/App.js
@@ -15,7 +15,6 @@ import ExerciseDataScienceOverview from "pages/module3/ExerciseDataScienceOvervi
 
 import CourseGetData from "pages/module4/CourseGetData";
 import ExerciseGetData from "pages/module4/ExerciseGetData";
-
 import "App.css";
 
 function App() {
